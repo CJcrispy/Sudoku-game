d4d21493df8c7b7b2fb7a48b3ee94ee8043f1ab3
CJcrispy <49406764+CJcrispy@users.noreply.github.com>
2020-09-22 17:29:35 -0400

    refactoring code

--- src/sudoku.py
+++ src/sudoku.py
@@ -1,7 +1,7 @@
 import pygame
 import time
 import random
- 
+from sudokuLogic import *
 pygame.font.init()
  
 display_width = 800
@@ -9,8 +9,12 @@ display_height = 600
  
 black = (0,0,0)
 white = (255,255,255)
-red = (255,0,0)
+red = (200,0,0)
+green = (0,200,0)
  
+bright_red = (255,0,0)
+bright_green = (0,255,0)
+
 block_color = (53,115,255)
  
 car_width = 73
@@ -49,6 +53,22 @@ def message_display(text):
  
     game_loop()
     
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("comicsansms",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
     
  
 def crash():
@@ -71,6 +91,13 @@ def game_intro():
         TextSurf, TextRect = text_objects("Sudoku", largeText)
         TextRect.center = ((display_width/2),(display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
+
+
+        button("Blank Board",150,450,100,50,green,bright_green,sudokuBlank)
+        button("Play",550,450,100,50,red,bright_red,sudokuFilled)
+
+        pygame.draw.rect(gameDisplay, red,(550,450,100,50))
+
         pygame.display.update()
         clock.tick(15)
         
